5152f722d72517aba00f96980ec117a270950ff7
Sebastian Asprino <asprinosebastian@proton.me>
2024-03-31 14:58:00 -0500

    api api trae los datos de la api original cuando se ejecutan sus funciones

--- src/pokemon_api/controllers/api/api.py
+++ src/pokemon_api/controllers/api/api.py
@@ -10,48 +10,8 @@ def GETApiPokemon():
   return pokemons
 
 
-test =  GETApiPokemon()
-print(test)
 
-# import requests
-
-# def get_api_pokemon():
-#     requests = 500  # Los Pokémon llegan por ID; actualmente estoy trayendo solo 500
-#     pokemons = []
-#     for id in range(1, requests + 1):
-#         response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
-#         pokemon_data = response.json()
-#         pokemons.append(pokemon_data)
-#     return pokemons
-
-# def get_api_type():
-#     response = requests.get('https://pokeapi.co/api/v2/type')
-#     types_data = response.json()
-#     return types_data
-
-
-
-# const fetch = require("node-fetch");
-
-# const GETApiPokemon = async () => {
-#   const Requests = 500; // los pokemos llegan por ID actualmente estoy trayendo solo 500
-#   const Pokemons = [];  
-#   for (let id = 1; id <= Requests; id++) {
-#     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-#     .then(response => response.json());
-#     Pokemons.push(response);
-#   }
-#   return Pokemons;
-# };
-
-# const GETApiType = async () => {
-#   const Types = await fetch(
-#     'https://pokeapi.co/api/v2/type'
-#   ).then((response) => response.json());
-#   return Types;
-# };
-
-# module.exports = { 
-#   GETApiPokemon,
-#   GETApiType
-# };
+def GETApiType():
+  response = requests.get('https://pokeapi.co/api/v2/type')
+  types_data = response.json()
+  return types_data
